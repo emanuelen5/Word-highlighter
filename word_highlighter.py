ddd23b61f1f214a5bdf16f776e091ad13d0ba078
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-03-25 22:43:29 +0200

    Getting words from selections
    - Managed to get the words from the current selections in the
      buffer
    - Added some TODO-steps in the code

--- word_highlighter.py
+++ word_highlighter.py
@@ -1,16 +1,25 @@
 import sublime
 import sublime_plugin
 
+# Add some base colors to use for selections (perhaps read from settings file)
+SCOPE_COLORS = [
+]
+
 class wordHighlighterHighlightInstancesOfSelection(sublime_plugin.TextCommand):
     """
     Highlights all instances of a specific word that is selected
     """
     def run(self, edit):
-        '''
-        This is run on the actual
-        '''
-        print("What is this \"edit\"?: " + str(edit))
-        print("Now it is actully running!")
+        text_selections = [self.view.substr(s) for s in self.view.sel()]
+        # Get unique items
+        text_selections = list(set(text_selections))
+
+        # TODO:
+        # 1. Get the words from each selection
+        #    Empty selections are expanded to words, nonempty are used as-is
+        # 2. Get a unique color association for each selection
+        # 3. Find all of the matching words
+        # 4. Use add_region() to set the scope for the regions
 
     def expand_selections_to_words(self):
         '''
