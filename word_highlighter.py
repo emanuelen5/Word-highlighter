c28e83d1234360d10c3ede021d52d6cfde5a647d
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-03-26 20:24:47 +0200

    Expanding empty selections to words
    - Found some inspiration in Default.DeleteWordCommand()

--- word_highlighter.py
+++ word_highlighter.py
@@ -21,22 +21,47 @@ class wordHighlighterHighlightInstancesOfSelection(sublime_plugin.TextCommand):
     """
     Highlights all instances of a specific word that is selected
     """
+    def expand_to_word(self, point):
+        # Stop for anything but subwords
+        stop_classes = sublime.CLASS_WORD_START | sublime.CLASS_WORD_END | sublime.CLASS_PUNCTUATION_START | sublime.CLASS_PUNCTUATION_END | sublime.CLASS_LINE_START | sublime.CLASS_LINE_END | sublime.CLASS_EMPTY_LINE
+        back_stop = self.view.find_by_class(point, forward=False, classes=stop_classes)
+        forward_stop = self.view.find_by_class(point, forward=True, classes=stop_classes)
+        r = sublime.Region(back_stop, forward_stop)
+        # Check that the found Region contains a word
+        if bits_set(self.view.classify(back_stop), sublime.CLASS_WORD_START) and bits_set(self.view.classify(forward_stop), sublime.CLASS_WORD_END):
+            # Valid word!
+            return r
+        else:
+            print("Expanded word is invalid: '{}'".format(self.view.substr(r)))
+            return sublime.Region(point, point) # Empty region
+
     def run(self, edit):
         text_selections = []
         for s in self.view.sel():
             if s.empty():
-                p = s.begin() # A point is just an index
-                classification = self.view.classify(p)
+                point = s.begin() # A point is just an index
+                classification = self.view.classify(point)
                 # If start of word, expand right to end of word
                 if bits_set(classification, sublime.CLASS_WORD_START):
                     print("At start of word!")
+                    back_stop = point
+                    forward_stop = self.view.find_by_class(point, forward=True, classes=sublime.CLASS_WORD_END)
+                    r = sublime.Region(back_stop, forward_stop)
                 # If end of word, expand left to start of word
                 elif bits_set(classification, sublime.CLASS_WORD_END):
                     print("At end of word!")
+                    back_stop = self.view.find_by_class(point, forward=False, classes=sublime.CLASS_WORD_START)
+                    forward_stop = point
+                    r = sublime.Region(back_stop, forward_stop)
                 # Else, expand left and right until hitting word start/end or punctuation.
                 # If the word start and end matches first, in right order use as word
                 else:
-                    pass
+                    r = self.expand_to_word(point)
+                # Append the word if it is not empty
+                txt = self.view.substr(r)
+                if txt != '':
+                    print("Expanded word is valid: '{}'".format(txt))
+                    text_selections.append(txt)
             else:
                 text_selections.append(self.view.substr(s))
         # Get unique items
@@ -49,9 +74,3 @@ class wordHighlighterHighlightInstancesOfSelection(sublime_plugin.TextCommand):
         # 2. Get a unique color association for each selection
         # 3. Find all of the matching words
         # 4. Use add_region() to set the scope for the regions
-
-    def expand_selections_to_words(self):
-        '''
-        Expand all of the current selections to "words" instead
-        '''
-        pass
