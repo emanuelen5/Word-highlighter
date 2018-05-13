3bcc6b1e8956a0ee04ce9fa9386be4360c843f6e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-05-13 23:43:50 +0200

    Testing several strings with non-fatal tests
    - This way it becomes easier to get an overview of for which
      characters the test fails for.

--- tests/word_highlighter_tests.py
+++ tests/word_highlighter_tests.py
@@ -8,18 +8,23 @@ class TestHighlighting(unittest.TestCase):
         self.window = sublime.active_window()
         self.view = self.window.new_file()
         self.view.set_scratch(True)
+        self.error_list = []
 
     def tearDown(self):
         self.view.close()
+        self.assertEqual([], self.error_list)
 
     def check_character(self, c):
         self.view.run_command("test_word_highlighter_add_character", c)
-        self.assertTrue(False)
+        self.assertTrue(False, "Not implemented yet")
 
     def test_highlight_characters(self):
         chars = ["<", ">", "'"]
         for i, c in enumerate(chars):
-            self.check_character(c)
+            try:
+                self.check_character(c)
+            except AssertionError as e:
+                self.error_list.append("{} : Highlighting for '{}' ".format(e, c))
 
 class TestWordHighlighterAddCharacterCommand(sublime_plugin.TextCommand):
     def run(edit, c):
