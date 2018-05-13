003bdb38541549aaba782f793bae431b42e1ba2b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-05-13 23:27:57 +0200

    Starting on testing framework for sublime commands
    - A new view is created and closed for each test case. Somehow we
      must also edit the view and run the application commands in it.

--- tests/word_highlighter_tests.py
+++ tests/word_highlighter_tests.py
@@ -1,12 +1,33 @@
 import unittest
 #from ..word_highlighter import ...
+import sublime
+import sublime_plugin
 
-class TestSomething(unittest.TestCase):
-    def test_dummy(self):
-        pass
+class TestHighlighting(unittest.TestCase):
+    def setUp(self):
+        self.window = sublime.active_window()
+        self.view = self.window.new_file()
+        self.view.set_scratch(True)
+
+    def tearDown(self):
+        self.view.close()
+
+    def check_character(self, c):
+        self.view.run_command("test_word_highlighter_add_character", c)
+        self.assertTrue(False)
+
+    def test_highlight_characters(self):
+        chars = ["<", ">", "'"]
+        for i, c in enumerate(chars):
+            self.check_character(c)
+
+class TestWordHighlighterAddCharacterCommand(sublime_plugin.TextCommand):
+    def run(edit, c):
+        print("TestWordHighlighterAddCharacterCommand.run()")
+        self.view.insert(edit, 0, c)
 
 def create_test_suite():
-    test_cases = (TestSomething, )
+    test_cases = (TestHighlighting, )
     suite = unittest.TestSuite()
     for test_class in test_cases:
         tests = unittest.TestLoader().loadTestsFromTestCase(test_class)
