ca39ba521836c616f4c10c475c1c08a64e87ba5c
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-06-03 13:34:09 +0200

    Adding example call of internally declarated function

--- tests/test_hw.py
+++ tests/test_hw.py
@@ -40,11 +40,14 @@ class TestWordHighlighter(TestCase):
         first_row = self.getRow(0)
         self.assertEqual(first_row, "new hello world")
 
+    def test_run_internal_function(self):
+        test_str = "new text with unique words"
+        self.setText(test_str)
+        word_highlighter.expand_to_word(self.view, 0)
+
 
 # for testing internal function
 if version < '3000':
-    # st2
-    Word_Highlighter = sys.modules["word_highlighter"]
+    word_highlighter = sys.modules["word_highlighter"]
 else:
-    # st3
-    Word_Highlighter = sys.modules["Word-highlighter.word_highlighter"]
+    word_highlighter = sys.modules["Word-highlighter.word_highlighter"]
