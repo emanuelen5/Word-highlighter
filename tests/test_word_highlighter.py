429ecc6a2e00e6b58eeeef1fe2273e1ad368cf3b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-06-03 17:03:57 +0200

    Clearing upon entry to fix many test-case errors
    - The old highlights remained due to early exit on error

--- tests/test_word_highlighter.py
+++ tests/test_word_highlighter.py
@@ -1,13 +1,16 @@
 import unittest
 import sublime
 
+def region_to_list(region):
+    assert isinstance(region, sublime.Region)
+    return [region.begin(), region.end()]
+
 class TestHighlighting(unittest.TestCase):
     def setUp(self):
         self.window = sublime.active_window()
         self.view = self.window.new_file()
         self.view.set_scratch(True)
         self.error_list = []
-        self.view.run_command("word_highlighter_clear_instances")
 
     def tearDown(self):
         self.view.close()
@@ -20,18 +23,19 @@ class TestHighlighting(unittest.TestCase):
         s.add(sublime.Region(0,1))
         # Highlight the selection
         self.view.run_command("word_highlighter_highlight_instances_of_selection")
-        regions = self.view.get_regions("word_highlighter.color0")
-        self.assertEqual([sublime.Region(0,1)], list(regions), "The first word should be highlighted")
-        self.view.run_command("word_highlighter_clear_instances")
+        regions = self.view.get_regions(word_highlighter.SCOPE_COLORS[0])
+        self.assertEqual(1, len(regions), "A word should be highlighted")
+        self.assertEqual(region_to_list(sublime.Region(0,1)), region_to_list(regions[0]), "The first word should be highlighted")
 
     def test_highlight_characters(self):
         chars = [chr(i) for i in range(0x20, 0x7f)]
         chars += ['\r', '\n']
         for i, c in enumerate(chars):
             try:
+                self.view.run_command("word_highlighter_clear_instances")
                 self.check_character(c)
             except AssertionError as e:
-                self.error_list.append(c)
+                self.error_list.append("'{}' - Error: '{}'".format(c,e))
         self.maxDiff = None
         self.assertEqual([], self.error_list, "Non-highlightable characters: Errors for {}/{}".format(len(self.error_list), len(chars)))
 
