a89006a286eeab0c546608107e8261e9507e6af1
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-08-29 22:07:12 +0200

    Test for clearing several words

--- tests/test_word_highlighter.py
+++ tests/test_word_highlighter.py
@@ -165,6 +165,25 @@ class TestWordHighlighterClearMenu(WordHighlighter_TestCase):
             self.wordHighlighterClearMenu._run()
         self.assertEqual(1, len(show_quick_panel_mock.mock_calls))
 
+    def test_clearing_all_words(self):
+        # Add some words and highlight them
+        self.set_buffer("word1 word2 word3")
+        self.collection._add_word(word_highlighter.WordHighlight("word1", match_by_word=True))
+        self.collection._add_word(word_highlighter.WordHighlight("word2", match_by_word=True))
+        self.collection._add_word(word_highlighter.WordHighlight("word3", match_by_word=True))
+        self.view.settings().set("wordhighlighter_collection", self.collection.dumps())
+
+        # Mocks the quick panel call and returns index of last item
+        def show_quick_panel_mock_call__select_last_item(items, callback, *args, selected_index=0, **kwargs):
+            return callback(clip(-1, selected_index, len(items)-1))
+
+        # Do the actual testing
+        with patch.object(self.wordHighlighterClearMenu.view, "window") as mock_window_method:
+            show_quick_panel_mock = MagicMock(side_effect=show_quick_panel_mock_call__select_last_item)
+            mock_window_method.return_value=MagicMock(show_quick_panel=show_quick_panel_mock)
+            self.wordHighlighterClearMenu._run()
+        self.assertEqual(4, len(show_quick_panel_mock.mock_calls))
+
 ## For testing internal functions
 import sys
 version = sublime.version()
